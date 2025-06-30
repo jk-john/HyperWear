e27af7ffa320c8bfed919e2ec843099f882a29bb
Jean Hendy <jeanyikikh@gmail.com>
2025-06-30 14:57:30 +0200

    feat: enhance AuthLayout by adding Toaster and AuthToast components for improved user notifications

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,19 @@
+import { AuthToast } from "@/components/ui/AuthToast";
+import { Toaster } from "@/components/ui/sonner";
+import { Suspense } from "react";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-full items-center justify-center">{children}</div>
+    <div className="flex h-full items-center justify-center">
+      {children}
+      <Toaster />
+      <Suspense>
+        <AuthToast />
+      </Suspense>
+    </div>
   );
 }
